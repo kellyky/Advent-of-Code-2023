0554a5188242c84817fe88efd2c64abf8b7d211a
Kelly Young <kelpopko@gmail.com>
2023-12-03 20:55:39 -0600

    Add solution for day 2 part 2: Cube Conundrum

--- day_two/day_two_part_two_cube_conundrum.rb
+++ day_two/day_two_part_two_cube_conundrum.rb
@@ -1,21 +1,8 @@
 require 'pry-byebug'
 games = File.read('input.txt').split("\n")
 
-# sample_games = [
-#   "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-#   "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-#   "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-#   "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-#   "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
-# ]
-
-#  Which games would have been possible if the bag had been loaded with only 12 red cubes, 13 green cubes, and 14 blue cubes. 
-#  What is the sum of the IDs of those games?
-
 class CubeConundrum
   attr_reader :tally
-
-  CUBE_LIMITS = { red: 12, green: 13, blue: 14 }
 
   def initialize(games)
     @games = games
@@ -25,29 +12,33 @@ class CubeConundrum
   def parse_game
     @games.each do |game|
       game_id, game_scores = game.split(":")
-
       id = game_id.scan(/[0-9]/).join.to_i
+
       sets = game_scores.split(";")
-
-
-      if set_scores_within_lmnits?(sets)
-        @tally += id
-      end
+      cube_products(sets)
     end
-    @tally
   end
 
-  def set_scores_within_lmnits?(sets)
-    sets.all? do |set|
+  def initialize_or_reset_cube_count
+    @cubes = { red: 0, green: 0, blue: 0 }
+  end
+
+  def cube_products(sets)
+    initialize_or_reset_cube_count
+    sets.map do |set|
       color_scores = set.split(",")
-      color_scores_within_limits?(color_scores)
+      cubes_needed(color_scores)
     end
+
+    @tally += @cubes.values.reduce { |product, score| product * score }
   end
 
-  def color_scores_within_limits?(color_scores)
-    color_scores.all? do |color_score|
+  def cubes_needed(color_scores)
+    color_scores.each do |color_score|
       numeric, color = color_score.split(" ")
-      numeric.to_i <= CUBE_LIMITS[color.to_sym]
+      if numeric.to_i > @cubes[color.to_sym]
+        @cubes[color.to_sym] = numeric.to_i
+      end
     end
   end
 end
