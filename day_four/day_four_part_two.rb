6981952a8baf2f128f47f9702857b8528d5e2ca7
Kelly Young <kelpopko@gmail.com>
2023-12-05 20:03:51 -0600

    Add day 2 part 2 solution

---
+++ day_four/day_four_part_two.rb
@@ -0,0 +1,51 @@
+require 'pry-byebug'
+cards = File.read('input.txt').split("\n")
+
+class ScratchCardPileReader
+  attr_reader :card_copies, :id
+
+  def initialize(cards)
+    @cards = cards
+    @card_stack = {}
+  end
+
+  def parse_cards
+    @cards.each do |original_card|
+      parse_raw_card_values(original_card)
+    end
+    evaluate_cards
+    count_card_stack
+  end
+
+  def parse_raw_card_values(original_card)
+    id, card_values = original_card.split(":")
+    @id = id.scan(/\d+/).first.to_i
+    winning_number_list, card_numbers = card_values.split("|")
+    @card_stack[@id] = { :copies => 1, :winning_numbers => winning_number_list.split, :card_values => card_numbers.split } 
+  end
+
+  def evaluate_cards
+    @card_stack.each do |card_number, card_info|
+      win_count = []
+
+      card_info[:copies].times do
+        win_count = winning_quantity(card_info[:winning_numbers], card_info[:card_values])
+        win_count.each.with_index do |winning_card, i|
+          @card_stack[card_number + i + 1][:copies] += 1
+        end
+      end
+    end
+  end
+
+  def winning_quantity(winning_numbers, card_numbers)
+    winning_numbers.select { |num| card_numbers.include?(num) }
+  end
+
+  def count_card_stack
+    @card_stack.sum { |card_number, card_info| card_info[:copies] }
+  end
+end
+
+
+lotto = ScratchCardPileReader.new(cards)
+puts lotto.parse_cards
